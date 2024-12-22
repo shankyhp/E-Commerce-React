9b8259e6cfc3f5223157f27ea65b550c299700e9
shankyhp <birajdarhpshankar@gmail.com>
2024-12-23 00:56:32 +0530

    completed frontend

--- E-Commerce/src/pages/Collection.jsx
+++ E-Commerce/src/pages/Collection.jsx
@@ -5,7 +5,7 @@ import Title from '../components/Title';
 import ProductItem from '../components/ProductItem';
 
 const Collection = () => {
-    const { products } = useContext(ShopContext);
+    const { products, search, showSearch } = useContext(ShopContext);
     const [showFilter, setShowFilter] = useState(false);
 
     const [filterProducts, setFilterProducts] = useState([]);
@@ -34,6 +34,13 @@ const Collection = () => {
     const applyFilter = () => {
 
         let productsCopy = products.slice();
+
+        if (showSearch && search) {
+            productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+
+        }
+
+
         if (category.length > 0) {
             productsCopy = productsCopy.filter(item => category.includes(item.category))
         }
@@ -65,7 +72,7 @@ const Collection = () => {
     useEffect(() => {
 
         applyFilter()
-    }, [category, subCategory])
+    }, [category, subCategory, search, showSearch])
 
     useEffect(() => {
         sortProduct()
